2c237b18451f4ae16caa092980276ff83234cf41
Kirill Sofronov <2138870+sokirill@users.noreply.github.com>
2022-03-18 00:06:37 +0800

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,39 @@
+<script>
+	let people = [
+		{ name: "yoshi", beltColour: "black", age: 25, id: 1 },
+		{ name: "mario", beltColour: "orange", age: 45, id: 2 },
+		{ name: "luigi", beltColour: "brown", age: 35, id: 3 }
+	]
+
+	const handleClick = (id) => {
+		// delete person from people
+		people = people.filter((person) => person.id != id)
+	}
+</script>
+
+<main>
+	{#each people as person (person.id)}
+		<div>
+			<h4>{person.name}</h4>
+			<p>{person.age} years old, {person.beltColor} belt</p>
+			<button on:click={() => handleClick(person.id)}>DELETE</button>
+		</div>
+	{:else}
+		<p>There are no people to show</p>
+	{/each}
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
